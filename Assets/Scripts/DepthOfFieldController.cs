279d394eed2d4710533a7b2e7b6163120a1d7005
Rod Ellison <rod.r.ellison@gmail.com>
2020-10-22 17:01:52 -0400

    upgrading project to Unity 2019.4.11f, revising DOF script to be able to try both Guassian and Bokeh modes

---
+++ Assets/Scripts/DepthOfFieldController.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class DepthOfFieldController : MonoBehaviour
+{
+    private Ray raycast;
+    private RaycastHit hit;
+    private bool isHit;
+    private float hitDistance;
+    public Volume volume;
+    public float focusSpeed;
+
+    private DepthOfField _depthOfField;
+
+    private bool _dofModeBokeh;
+    private bool _dofModeGuassian;
+    public bool drawGizmosForDebug;
+
+
+    public void SetGuassian()
+    {
+        _depthOfField.mode.value = DepthOfFieldMode.Gaussian;
+        _dofModeBokeh = false;
+        _dofModeGuassian = true;
+    }
+
+    public void SetBokeh()
+    {
+        _depthOfField.mode.value = DepthOfFieldMode.Bokeh;
+        _dofModeGuassian = false;
+        _dofModeBokeh = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volume.profile.TryGet<DepthOfField>(out _depthOfField);
+
+        switch (_depthOfField.mode.value)
+        {
+            case DepthOfFieldMode.Bokeh:
+                _dofModeBokeh = true;
+                break;
+            case DepthOfFieldMode.Gaussian:
+                _dofModeGuassian = true;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.G))
+            SetGuassian();
+        if (Input.GetKey(KeyCode.B))
+            SetBokeh();
+
+        isHit = false;
+
+        raycast = new Ray(transform.position, transform.forward * 100f);
+        if (Physics.Raycast(raycast, out hit, 100f))
+        {
+            isHit = true;
+            hitDistance = Vector3.Distance(transform.position, hit.point);
+            //      Debug.Log("Hit");
+        }
+        else
+        {
+            if (hitDistance < 100f)
+            {
+                hitDistance++;
+            }
+        }
+
+        SetFocus();
+    }
+
+    void SetFocus()
+    {
+        Debug.Log("Setting focal distance to " + hitDistance);
+        if (_dofModeBokeh)
+        {
+            _depthOfField.focusDistance.value = Mathf.Lerp(_depthOfField.focusDistance.value, hitDistance,
+                Time.deltaTime * focusSpeed);
+        }
+        else if (_dofModeGuassian)
+        {
+            if (hitDistance <= 5)
+                _depthOfField.gaussianStart.value = Mathf.Lerp(_depthOfField.gaussianStart.value, 6f,
+                    Time.deltaTime * focusSpeed);
+            else
+            {
+                _depthOfField.gaussianStart.value = Mathf.Lerp(_depthOfField.gaussianStart.value,
+                    hitDistance + hitDistance * 0.25f,
+                    Time.deltaTime * focusSpeed);
+                _depthOfField.gaussianEnd.value = _depthOfField.gaussianStart.value + hitDistance * 2f;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!drawGizmosForDebug)
+            return;
+
+        if (isHit)
+        {
+            Gizmos.DrawSphere(hit.point, 0.1f);
+            Debug.DrawRay(transform.position,
+                transform.forward * Vector3.Distance(transform.position, hit.point));
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.forward * 100f);
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -1,9 +1,13 @@
 ### **LWRP-Shadergraph-DOF**
 
-A Unity3D Test project, experimenting with the default LWRP setup and playing around with a few different techniques:
+**Note**: No longer **LWRP** (Light-weight Render pipeline), updated 
+project to be 2019.4.11f **URP** (Universal Rendering Pipeline)
+
+A Unity3D Test project, playing around with a few different techniques:
 1. Foliage movement and animation using Shadergraph
     - improving default 
-2. Scripted Depth of Focus changes [from keijiros github project ](https://github.com/keijiro/PostProcessingUtilities) 
+2. Scripted Depth of Focus changes
+    See Scripts/DepthOfFieldController.cs
 3. Dynamic plane generation ([from Brackeys tutorial](https://www.youtube.com/watch?v=64NblGkAabk))
 3. Enhancing the Ocean shader (from the Unity tutorial - [Creating Animated materials with Shadergraph](https://blogs.unity3d.com/2018/10/05/art-that-moves-creating-animated-materials-with-shader-graph/))
 
@@ -20,8 +24,12 @@ This project utilizes a few free resources, attributed here:
 ![Beach1](LWRP-Beach1.JPG "Beach view 1")
 ![Beach2](LWRP-Beach2.JPG "Beach view 2")
 
+### When running project:
+1. Press 'G' to see Guassian DOF effect, press 'B' to see Bokeh effect.
+
 ###### **To Do**
 - Ocean foam at beach terrain intersection
+    OR - try other people's better Water shaders..
 - VFX for sand surface particles
 - Add a boat, and swaying rope
 - Try to use Shadergraph to animate birds and crabs
